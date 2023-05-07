03daf50f80f6223145dea1ad2fc9a417b5ce119f
Raju <ashfaqraju27@gmail.com>
2023-05-08 01:16:38 +0600

    professional section added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import ServiceCard from './ServiceCard/ServiceCard';
+import ProfessionalSection from './ProfessionalSection/ProfessionalSection';
 
 const Home = () => {
     return (
         <div className='mx-5'>
             <Banner></Banner>
             <ServiceCard></ServiceCard>
+            <ProfessionalSection></ProfessionalSection>
         </div>
     );
 };
---
+++ src/pages/Home/Home/ProfessionalSection/ProfessionalSection.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import photo from '../../../../assets/images/engin-akyurt-g-m8EDc4X6Q-unsplash 1.png'
+
+const ProfessionalSection = () => {
+    return (
+        <div className="mt-14 hero min-h-screen bg-base-200 rounded-xl">
+            <div className="hero-content flex-col lg:flex-row">
+                <img src={photo} alt='' className="max-w-2xl rounded-lg shadow-2xl" />
+                <div>
+                    <h1 className="text-4xl font-bold mb-5">
+                        Let us handle your screen <span className='text-primary'>Professionally</span>.
+                    </h1>
+                    <p className="py-6 mt-6 ">Spa services can include massage, along with spa treatments such as mud baths. High-end spa/salons may provide packages which allow people to purchase a bundle of services for an event such as a wedding, with some spas allowing wedding parties to take over the salon for a day to get an array of beauty services before the wedding.</p>
+                    <div className='mt-10 mx-28 flex justify-between'>
+                        <div>
+                            <h1 className='text-5xl font-bold text-primary'>500+</h1>
+                            <p className='mt-2'>Happy Customer</p>
+                        </div>
+                        <div>
+                            <h1 className='text-5xl font-bold text-primary'>16+</h1>
+                            <p className='mt-2'>Total Service</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProfessionalSection;
